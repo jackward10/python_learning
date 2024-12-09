950f6446a150bbd43522e0222156ee7a35598560
beeplovebhandari <bbhandari0304@gmail.com>
2024-12-09 21:27:59 +0545

    map,filter,reduce

---
+++ Day_18/map.py
@@ -0,0 +1,19 @@
+# map()
+# map() is a buitl-in function that takes two parameters as an input. First parameter is function
+# and second parameter is an iterable
+# map() function changes every element in an iterable to some other form
+
+nums = [1, 2, 3, 4, 5]
+def increase_by_10(data):
+    return data +10
+
+def increase_by_power(a):
+    return a ** 3
+
+
+result = map(increase_by_10, nums)
+print(nums)
+print(list(result))
+
+r = map(increase_by_power, nums)
+print(list(r))
